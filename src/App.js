49993ca7d909b482bbba88af4dbef30b5e80d6a8
roohitgupta <96112427+roohitgupta@users.noreply.github.com>
2022-06-29 23:36:24 +0530

    added Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
+import NavBar from "./components/NavBar"
 
 
 const App = () => {
   return (
     <div>
-     <h1 className='font-bold underline text-4xl font-signature'>
-      Hello world! hiiii
-    </h1>
+     <NavBar />
+
     </div>
   )
 }
