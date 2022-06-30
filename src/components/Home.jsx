0c81b7b05bfb6b6d85502b7394e580537aa6e27e
roohitgupta <96112427+roohitgupta@users.noreply.github.com>
2022-06-30 15:47:30 +0530

    added Introduction Part with Image div

---
+++ src/components/Home.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { BiChevronRight } from 'react-icons/bi';
+import heroImage from '../assets/Rohit_Gupta.jpeg'
+
+const Home = () => {
+  return (
+    <div name="home" className="h-screen w-full bg-gradient-to-b from-black to-gray-800">
+
+      <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+
+        <div className='flex flex-col justify-center h-full'>
+            <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Full Stack Developer</h2>
+            <p className='text-gray-500 py-4 max-w-md'>I have 8 yars of experience building and         desgining software. 
+            Currently, I love to work on web application using technologies like React, Tailwind, Node.js and MongoDb.
+            </p>
+
+            <div>
+                <button>
+                    Portfolio 
+                    <span>
+                        <BiChevronRight />
+                    </span>
+                </button>
+            </div>
+        </div>
+        <div>
+            <img src={heroImage} alt="My Profile" className='rounded-2xl mx-auto w-2/3 md:w-full' />
+        </div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Home
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -36,9 +36,8 @@ const NavBar = () => {
             </div>
 
             <ul className='hidden md:flex'>
-
                 {links.map(({ id, link }) => (
-                    <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'>{link}</li>
+                    <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-110 duration-200'>{link}</li>
                 ))}
             </ul>
 
@@ -47,12 +46,10 @@ const NavBar = () => {
             </div>
 
             {nav && (
-
                 <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
                     {links.map(({ id, link }) => (
                         <li key={id} className='px-4 cursor-pointer capitalize py-6 text-4xl'>{link}</li>
                     ))}
-
                 </ul>
 
             )}
